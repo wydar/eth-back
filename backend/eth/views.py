26bda9d3818f41376e7851ee58dccf34afb0f27b
Pablo <pablo96ga@gmail.com>
2022-05-10 23:14:53 +0200

    clean views.py

--- backend/eth/views.py
+++ backend/eth/views.py
@@ -13,76 +13,6 @@ from eth.models import Station  # , Measures
 from backend.settings import BLOCKCHAIN_SERVER, ABI, BYTE_CODE, w3
 
 
-# class StationViewSet2(viewsets.ModelViewSet):
-#     """
-#     A simple ViewSet for viewing and editing Stations.
-#     """
-#     queryset = Station.objects.all()
-#     serializer = StationSerializer
-
-
-# class MeasuresViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
-#     """
-#     A simple ViewSet for viewing and editing Measures.
-#     """
-#     queryset = Measures.objects.none()
-#     serializers = MeasureSerializer
-
-#     @staticmethod
-#     def parseMeasures(station, measures, response):
-#         """
-#         Function to parse the blockchain response into JSON format
-#         """
-#         temperatures, altitudes, humidities, preassures, timestamps = measures
-
-#         response_aux = {}
-#         num = 0
-#         for temp, alt, hum, pre, tim in zip(temperatures, altitudes, humidities, preassures, timestamps):
-#             response_aux[num] = {
-#                 'temperaure': temp/100,
-#                 'altitude': alt/100,
-#                 'humidity': hum/100,
-#                 'preassure': pre/100,
-#                 'timestamp': tim
-#                 }
-#             num += 1
-
-#         response_aux[station.station_id] = response_aux
-#         return json.loads(json.dumps(response))
-
-#     def list(self, request, *args, **kwargs):
-#         """
-#         Endpoint to connect with the blockchain and retrieve last measure of all stations
-#         """
-#         stations = Station.objects.filter('is_active'==True)
-#         response_data = {}
-#         for station in stations:
-#             contract = w3.eth.contract(address=station.contract_adress, abi=station.contract_abi)
-#             measure = contract.functions.getMeasures(1).call()
-#             response_data = MeasuresViewSet.parseMeasures(station, measure, response_data)
-#         return Response(data=response_data, status=status.HTTP_200_OK)
-
-#     def station(self, request, *args, **kargs):
-#         """
-#         Endpoint to connect with the blockchain and retrieve measures of one station
-#         """
-#         w3 = Web3(Web3.HTTPProvider(BLOCKCHAIN_SERVER))
-#         w3.eth.defaultAccount = w3.eth.accounts[1]
-#         contract = w3.eth.contract(address='', abi=ABI)
-
-#         timestamp = request.GET.get('date')
-
-#         if not timestamp:
-#             measure = contract.functions.getMeasures(1).call()
-#         else:
-#             try:
-#                 measure = contract.functions.getLastMeasuresFromTimestamp(int(timestamp)).call()
-#             except ValueError:
-#                 return Response(data='Bad date format', status=status.HTTP_400_BAD_REQUEST)
-#         response_data = MeasuresViewSet.parseMeasures(measure)
-#         return Response(data=response_data, status=status.HTTP_200_OK)
-
-
 class StationViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     queryset = Station.objects.all()
     serializer_class = StationSerializer
@@ -170,7 +100,6 @@ class StationViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
         stations = Station.objects.filter(is_active=True)
         response_data = {}
         for station in stations:
-            print(stations)
             contract = w3.eth.contract(address=station.contract_address, abi=station.contract_abi)
             measure = contract.functions.getMeasures(1).call()
             response_data[station.station_id] = StationViewSet.parseMeasures(station, measure)
@@ -191,7 +120,6 @@ class StationViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
         if not timestamp:
             measure = contract.functions.getMeasures(1).call()
             response_data = StationViewSet.parseMeasures(station, measure)
-            print(measure)
         else:
             try:
                 measure = contract.functions.getLastMeasuresFromTimestamp(int(timestamp)).call()
